5b0163fddab192e1ca7550e05014941ca8506308
rebeccajohnson88 <rebeccajohnson@Rebeccas-MacBook-Pro.local>
2021-06-29 17:57:35 -0400

    fix hardcoded path names; more renaming of address and date cols

--- code/02_RenameCol_Rowbind.py
+++ code/02_RenameCol_Rowbind.py
@@ -8,13 +8,16 @@ import os
 from os import listdir
 from pathlib import Path
 import re
+import numpy as np
 
 dirname = os.path.dirname(__file__)
 dropbox_general = str(Path(dirname).parents[1])
 DROPBOX_DATA_PATH = os.path.join(dropbox_general, 
                                  "qss20_finalproj_rawdata/summerwork/") 
 DROPBOX_RAW_PATH = os.path.join(DROPBOX_DATA_PATH,
-                                "raw/") 
+                                "raw/")
+DROPBOX_INT_PATH = os.path.join(DROPBOX_DATA_PATH,
+                                "intermediate/")  
 all_disclosure_files = [file for file in listdir(DROPBOX_RAW_PATH) if
                         "H_2A_Disclosure_Data" in file]
 
@@ -25,21 +28,30 @@ for file in all_disclosure_files:
     key_name = "df_" + re.findall("20[0-9][0-9]", file)[0]
     print("Reading file: " + file)
     disc_files[key_name] = pd.read_excel(DROPBOX_RAW_PATH + file)
+    
 
 # combine into a list
 df_2014 = disc_files['df_2014']
+df_2014['data_source'] = "file_2014"
 df_2015 = disc_files['df_2015']
+df_2015['data_source'] = "file_2015"
 df_2016 = disc_files['df_2016'] 
+df_2016['data_source'] = "file_2016"
 df_2017 = disc_files['df_2017']
+df_2017['data_source'] = "file_2017"
 df_2018 = disc_files['df_2018']
+df_2018['data_source'] = "file_2018"
 df_2019 = disc_files['df_2019'] 
+df_2019['data_source'] = "file_2019"
 df_2020 = disc_files['df_2020']
+df_2020['data_source'] = "file_2020"
 df_2021 = disc_files['df_2021']
+df_2021['data_source'] = "file_2021"
 
 
 full_data = [df_2014, df_2015, df_2016, df_2017, df_2018, df_2019, df_2020, df_2021]
 
-print(full_data[0].head())
+#print(full_data[0].head())
 ## get and print intersecting columns (before rename)
 colnames = [set(df_2014.columns), set(df_2015.columns), set(df_2016.columns), set(df_2017.columns), set(df_2018.columns), set(df_2019.columns), set(df_2020.columns), set(df_2021.columns)]
 intersecting_cols = list(set.intersection(*colnames))
@@ -90,35 +102,67 @@ df_2021['ATTORNEY_AGENT_NAME'] = df_2021[['ATTORNEY_AGENT_FIRST_NAME', 'ATTORNEY
 df_2020.rename(columns = {'EMPLOYER_ADDRESS_1': 'EMPLOYER_ADDRESS1'}, inplace = True)
 df_2021.rename(columns = {'EMPLOYER_ADDRESS_1': 'EMPLOYER_ADDRESS1'}, inplace = True)
 
+
 ## next to try to add back the job start and end date- pre-2020 it's 
 ## JOB_START_DATE and JOB_END_DATE
 ## after 2020 it becomes EMPLOYMENT_END_DATE AND EMPLOYMENT_START_DATE
 ## so rename those 
-for one_data in full_data:
-    print([col for col in one_data.columns if "YEAR" in col or "DATE" in col])
+df_2020.rename(columns = {'REQUESTED_BEGIN_DATE': 'REQUESTED_START_DATE_OF_NEED',
+                          'REQUESTED_END_DATE' : 'REQUESTED_END_DATE_OF_NEED',
+                          'EMPLOYMENT_BEGIN_DATE': 'JOB_START_DATE',
+                          'EMPLOYMENT_END_DATE': 'JOB_END_DATE'}, inplace = True)
+df_2021.rename(columns = {'REQUESTED_BEGIN_DATE': 'REQUESTED_START_DATE_OF_NEED',
+                          'REQUESTED_END_DATE' : 'REQUESTED_END_DATE_OF_NEED',
+                          'EMPLOYMENT_BEGIN_DATE': 'JOB_START_DATE',
+                          'EMPLOYMENT_END_DATE': 'JOB_END_DATE'}, inplace = True)
+df_2014.rename(columns = {'CERTIFICATION_BEGIN_DATE': 'JOB_START_DATE',
+                          'CERTIFICATION_END_DATE': 'JOB_END_DATE'}, inplace = True)
+df_2015['REQUESTED_START_DATE_OF_NEED'] = np.nan
+df_2015['REQUESTED_END_DATE_OF_NEED'] = np.nan
+
 
 ## compare intersecting columns (before and after rename)
 colnames_rename = [set(df_2014.columns), set(df_2015.columns), set(df_2016.columns), set(df_2017.columns), 
                    set(df_2018.columns), set(df_2019.columns), set(df_2020.columns), set(df_2021.columns)]
 intersecting_cols_rename = list(set.intersection(*colnames_rename))
 print("Originally there are " + str(len(intersecting_cols)) +" intersecting columns. After renaming and cleaning, there are " + str(len(intersecting_cols_rename)) + " intersecting columns." )
-print(intersecting_cols_rename)
+
 ## rowbind 2014-2021 H-2A data
-h2a_combined = pd.concat([df_2014, df_2015, df_2016, df_2017, df_2018, df_2019, df_2020, df_2021])
+## add indicator for what disclosure file it came from
+h2a_combined_fillblanks = pd.concat([df_2014, df_2015, df_2016, df_2017, df_2018, df_2019, df_2020, df_2021])
+print(h2a_combined_fillblanks.head())
 pd.set_option('display.max_columns', None)
-print("Combined datasets' columns: " + str(h2a_combined.columns.tolist()))
-print(h2a_combined.EXPOSURE_TO_TEMPERATURES)
-print([col for col in h2a_combined.columns if "WORKSITE" in col])
-print([col for col in h2a_combined.columns if "ADDRESS" in col])
+#print("Combined datasets' columns: " + str(h2a_combined_fillblanks.columns.tolist()))
+
+## Write different forms of the data
+h2a_combined_commoncols = h2a_combined_fillblanks[intersecting_cols_rename].copy()
+print(h2a_combined_commoncols.head()) 
+print("Combined datasets' columns that are non-blank are: " + str(h2a_combined_commoncols.columns.tolist()))
+
+## get intersecting columns just for 2020 and 2021
+intersect_2021 = list(set(df_2020.columns).intersection(df_2021.columns)) 
+h2a_combined_20202021 = pd.concat([df_2020, df_2021]) 
+h2a_combined_20202021_int = h2a_combined_20202021[intersect_2021].copy()
 
 
-### write one version that's all years; another with 2020 and 2021
-print([col for col in df_2015.columns if "ADDRESS" in col])
+## write different files
+### file set 1: jobs data across years restricted to columns common across years
+h2a_combined_commoncols.to_csv(DROPBOX_INT_PATH + "h2a_combined_2014-2021.csv",
+                        index = False)
+h2a_combined_commoncols.to_pickle(DROPBOX_INT_PATH + "h2a_combined_2014-2021.pkl")
 
-# ## write pkl and csv form to Dropbox folder
-# h2a_combined.to_csv(DROPBOX_DATA_PATH + "intermediate/h2a_combined_2014-2021.csv",
-#                         index = False)
-# h2a_combined.to_pickle(DROPBOX_DATA_PATH + "intermediate/h2a_combined_2014-2021.pkl")
+### file set 2: jobs data just 2020 and 2021 (since many more columns)
+h2a_combined_20202021_int.to_csv(DROPBOX_INT_PATH + "h2a_combined_2020-2021.csv",
+                        index = False)
+h2a_combined_20202021_int.to_pickle(DROPBOX_INT_PATH + "h2a_combined_2020-2021.pkl")
+
+## file set 3: jobs data all years/all columns (fills ones that don't exist with NA)
+h2a_combined_fillblanks.to_csv(DROPBOX_INT_PATH + "h2a_combined_2014-2021_preserveallcols.csv",
+                        index = False)
+h2a_combined_fillblanks.to_pickle(DROPBOX_INT_PATH + "h2a_combined_2014-2021_preserveallcols.pkl")
+
+
+
 
 # =============================================================================
 
