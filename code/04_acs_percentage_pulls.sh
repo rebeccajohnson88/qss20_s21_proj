c408702b28b5a0e7cd30404aa77c5c943ae365ad
eunice30718 <81632872+eunice30718@users.noreply.github.com>
2021-08-10 02:27:31 +0800

    bash script for percentage pulling

--- code/04_acs_percentage_pulls.sh
+++ code/04_acs_percentage_pulls.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/python bash
 for i in {2014..2019}
 do
-	python3 04_acs_demographics_percentage.py - -dropbox "/Users/euniceliu/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/" --acsyear $i
+	python3 04_acs_demographics_percentage.py --dropbox "/Users/euniceliu/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/" --acsyear $i
 done
 
 
