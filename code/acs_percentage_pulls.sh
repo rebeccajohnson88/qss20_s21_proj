f35630ef94d498a64d8ef60b8241144c1d58d33e
eunice30718 <81632872+eunice30718@users.noreply.github.com>
2021-08-10 02:04:26 +0800

    Delete acs_percentage_pulls.sh

--- code/acs_percentage_pulls.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python bash
-for i in {2014..2019}
-do
-	python3 04_acs_demographics_percentage.py - -dropbox "/Users/euniceliu/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/" --acsyear $i
-done
-
-
-
