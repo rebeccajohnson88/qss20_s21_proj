d07bfb96a679fea33a4a6bbafa1846405ef754de
camguage <71299048+camguage@users.noreply.github.com>
2021-08-13 16:33:58 -0400

    Update 07_outcome_variables.R

--- code/07_outcome_variables.R
+++ code/07_outcome_variables.R
@@ -24,68 +24,58 @@ setwd(DATA_DIR)
 #####################
 # Loading in Data
 #####################
-# approved_only before deduping
-approved_only_pre_deduping <- readRDS("intermediate/approved_only_pre_deduping.RDS")
-
-# investigations_filtered before deduping
-investigations_filtered_pre_deduping <- readRDS("intermediate/investigations_filtered_pre_deduping.RDS")
-
 # matched data with de-duped datasets
-matched_data <- readRDS("intermediate/fuzzy_matching_final.RDS")
+matched_data <- readRDS("intermediate/final_df.RDS")
+
+####################
+# Some descriptives
+####################
+matched_data_no_invesitgations <- matched_data %>%
+  filter(is_matched_investigations == FALSE)
+
+# dedupe jobs quick to check about percentage of employers with investigations
+set.seed(1)
+
+matched_data_deduped_byjob <- matched_data %>%
+  group_by(jobs_group_id) %>%
+  filter(DECISION_DATE == min(DECISION_DATE)) %>%
+  sample_n(1) 
+
+matched_data_deduped_byjob_invest <- matched_data_deduped_byjob %>%
+  filter(is_matched_investigations == TRUE)
+
+nrow(matched_data_deduped_byjob_invest)
 
 
-###################
-# Re-duplication
-###################
-
-# find the indices we need to replace 
-# make sure that x and y are correct
-indices_to_replace_approved_only <- setdiff(approved_only_pre_deduping$merging_index, matched_data$merging_index.y)
-indices_to_replace_investigations_filtered <- setdiff(investigations_filtered_pre_deduping$merging_index, matched_data$merging_index.x) 
-
-# grab the observations with those indices
-approved_only_discarded_obs <- approved_only_pre_deduping %>%
-  filter(merging_index %in% indices_to_replace_approved_only)
-
-investigations_filtered_discarded_obs <- investigations_filtered_pre_deduping %>%
-  filter(merging_index %in% indices_to_replace_investigations_filtered)
-
-# rename columns for merging
-approved_only_discarded_obs <- approved_only_discarded_obs %>%
-  rename(name.y = name,
-         city.y = city,
-         dedupe.ids.y = dedupe.ids,
-         merging_index.y = merging_index)
-
-
-# merge them back onto the dataset
-matched_data_intermediate <- merge(matched_data, approved_only_discarded_obs, all.x = TRUE)
-matched_data_final <- rbind(matched_data_intermediate, investigations_filtered_discarded_obs)
-
-# now this is tricky, we need to replace the na's of the duplicates with the actual match
+sprintf("Our matched data has %s rows, %s unique employers, has %s%% of rows with an investigation, and %s%% of employers with an investigation", 
+        nrow(matched_data), length(unique(matched_data$jobs_group_id)), 
+        (nrow(matched_data_no_invesitgations) / nrow(matched_data)) * 100,
+        (nrow(matched_data_deduped_byjob_invest) / nrow(matched_data_deduped_byjob)) * 100)
 
 
 #####################
 # Cleaning the Data
 #####################
 
-matched_deduped <- matched_deduped %>%
-  mutate(findings_start_date = ymd(findings_start_date)) %>%
-  mutate(JOB_START_DATE = ymd_hms(JOB_START_DATE))
-# will UTC thing in JOB_START_DATE present an issue?
+matched_data <- matched_data %>%
+  mutate(findings_start_date = mdy(findings_start_date)) %>%
+  mutate(findings_end_date = mdy(findings_end_date)) %>%
+  mutate(JOB_START_DATE = ymd_hms(JOB_START_DATE)) %>% 
+  mutate(JOB_END_DATE = ymd_hms(JOB_END_DATE))
+
+# Getting the following messages: 1: Problem with `mutate()` input `JOB_END_DATE`.ℹ  17415 failed to parse. ℹ Input `JOB_END_DATE` is `ymd_hms(JOB_END_DATE)`. 2:  17415 failed to parse. 
 
 #############################
 # Creating outcome variables
 #############################
 
 matched_data <- matched_data %>%
-  mutate(outcome_is_any_investigation = ifelse(!is.na(id.y), FALSE, TRUE)) %>%
-  mutate(outcome_is_investigation_aftersd = ifelse((!is.na(id.y) & findings_start_date >= JOB_START_DATE & findings_end_date > JOB_END_DATE), TRUE, FALSE)) %>%
-  mutate(outcome_is_investigation_before_sd = ifelse((!is.na(id.y) & findings_start_date < JOB_START_DATE), TRUE, FALSE)) %>%
-  mutate(outcome_is_investigation_overlapsd = ifelse((!is.na(id.y) & findings_start_date >= JOB_START_DATE & findings_start_date <= JOB_END_DATE), TRUE, FALSE))
+  mutate(outcome_is_any_investigation = is_matched_investigations) %>%
+  mutate(outcome_is_investigation_aftersd = ifelse(is_matched_investigations & findings_start_date >= JOB_START_DATE, TRUE, FALSE)) %>%
+  mutate(outcome_is_investigation_before_sd = ifelse(is_matched_investigations & findings_start_date < JOB_START_DATE, TRUE, FALSE)) %>%
+  mutate(outcome_is_investigation_overlapsd = ifelse(is_matched_investigations & findings_start_date >= JOB_START_DATE & findings_start_date <= JOB_END_DATE, TRUE, FALSE))
 
-table(matched_data$outcome_1)
-table(matched_data$outcome_2)
-table(matched_data$outcome_3)
-table(matched_data$outcome_4)
-
+table(matched_data$outcome_is_any_investigation)
+table(matched_data$outcome_is_investigation_aftersd)
+table(matched_data$outcome_is_investigation_before_sd)
+table(matched_data$outcome_is_investigation_overlapsd)
