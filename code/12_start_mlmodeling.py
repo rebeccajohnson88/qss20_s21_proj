10278c27240dae65d7f401960297840d7ba80983
General.Helen <helenma0223@gmail.com>
2021-08-21 19:43:59 -0400

    updates on preprocessing

--- code/12_start_mlmodeling.py
+++ code/12_start_mlmodeling.py
@@ -25,15 +25,16 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GroupShuffleSplit
 
 ##### Input Path
-DROPBOX_INT_PATH = '../../qss20_finalproj_rawdata/summerwork/intermediate/'
-DATA_FILE_NAME = "placeholderdata_formodeling.csv"
-H2A_DATA_FILE_NAME = "h2a_combined_2014-2021.pkl"
+DROPBOX_INT_PATH = '../../qss20_finalproj_rawdata/summerwork/'
+DATA_FILE_NAME = "/clean/jobs_formod.csv"
+H2A_DATA_FILE_NAME = "/intermediate/h2a_combined_2014-2021.pkl"
 DATA_FILE_PATH = os.path.join(DROPBOX_INT_PATH, DATA_FILE_NAME)
 H2A_DATA_FILE_PATH = os.path.join(DROPBOX_INT_PATH, H2A_DATA_FILE_NAME)
 # local path for now
-DATA_FILE_PATH2 = "/Users/Firstclass/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/intermediate/placeholderdata_formodeling.csv"
+DATA_FILE_PATH2 = "/Users/Firstclass/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/clean/jobs_formod.csv"
 H2A_DATA_FILE_PATH2 = "/Users/Firstclass/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/intermediate/h2a_combined_2014-2021.pkl"
 
 
@@ -41,12 +42,13 @@ H2A_DATA_FILE_PATH2 = "/Users/Firstclass/Dropbox (Dartmouth College)/qss20_final
 def extract_year_month(df, col):
 
     ## convert col into datetime
-    df[col] = pd.to_datetime(df[col])
+    df[col] = pd.to_datetime(df[col],errors='coerce')
 
     ## extract year and month into seperate columns
-    df[col+'_YEAR'] = df[col].dt.year
-    df[col+'_MONTH'] = df[col].dt.month
-    # df[col + '_YEAR_MONTH'] = df[col].dt.to_period('M')
+    df[col + '_YEAR_MONTH'] = df[col].dt.strftime('%Y-%m')
+    #df[col+'_YEAR'] = df[col].dt.year
+    #df[col+'_MONTH'] = df[col].dt.month
+
 
 def gen_topk_dummies(df, feature, percentThreshold):
 
@@ -91,13 +93,14 @@ with open(H2A_DATA_FILE_PATH2, "rb") as df:
 job_feature = h2a_data.columns.tolist()
 job_feature.append("jobs_group_id")
 
-#print(pre_df.columns.values)
+print(pre_df.columns.values)
+print(pre_df.info(verbose=True, show_counts=True))
 #print(h2a_data.columns.values)
 
 ## Assign outcome boolean vars to y (value we are trying to predict)
 outcomes = ['is_matched_investigations','outcome_is_investigation_overlapsd']
 print("Outcome variables to predict are:" + str(outcomes))
-y1 = list(pre_df[outcomes[0]])
+y1 = pre_df[outcomes[0]]
 y2 = list(pre_df[outcomes[1]])
 # remove them from the preMatrix ... because that would be too easy!
 pre_df = pre_df.drop(columns=outcomes, axis=1)
@@ -116,9 +119,9 @@ print(preMatrix.nunique(axis=0))
 
 ## seperate num/cat columns
 numeric_options = ["int64", "float64", "datetime64[ns]"]
+date_cols = ['JOB_END_DATE_YEAR_MONTH', 'JOB_START_DATE_YEAR_MONTH']
 num_cols = [one for one in preMatrix.columns if preMatrix.dtypes[one] in numeric_options] # all nums are actually dates
-cat_cols = [one for one in preMatrix.columns if preMatrix.dtypes[one] not in numeric_options]
-#date_cols = [one for one in preMatrix.columns if preMatrix.dtypes[one] == "datetime64[ns]"]
+cat_cols = [one for one in preMatrix.columns if (preMatrix.dtypes[one] not in numeric_options) & (one not in date_cols)]
 
 ## select cat columns with less than 1000 unique values
 # choosing columns w/ nrow/2 unique values
@@ -126,20 +129,22 @@ selected_cat_large = [c for c in cat_cols if preMatrix[c].nunique()<=1000]
 
 print('Numeric Columns:')
 print(num_cols)
+print('Merged Date Columns:')
+print(date_cols)
 print('\nSelected Categorical Columns:')
 print(selected_cat_large)
 
 
 ## change some row values to others according to value count of the column
-# what columns we want to change?
-preMatrix = gen_topk_dummies(preMatrix,"ATTORNEY_AGENT_NAME",0.01)
+for col in ['ATTORNEY_AGENT_CITY','ATTORNEY_AGENT_NAME','WORKSITE_CITY']:
+    preMatrix = gen_topk_dummies(preMatrix,col,0.01)
 
 # do a train test split
 # split into train and test sets (80/20)
 
-# split on job group id: 'jobs_group_id'
-# X_train, X_test, y_train, y_test = train_test_split(imputed_cat_feature_pre, y, test_size=0.20, random_state=1)
-X_train, X_test, y_train, y_test = train_test_split(preMatrix, y1, test_size=0.20, random_state=3)
+# group shuffle split on job group id: 'jobs_group_id'
+gs = GroupShuffleSplit(n_splits=2, train_size=.7, random_state=42)
+train_ix, test_ix = next(gs.split(preMatrix, y1, groups=preMatrix.jobs_group_id))
 
 #### data imputation
 
@@ -148,37 +153,33 @@ cat_feature_pre = preMatrix.loc[:, selected_cat_large].copy()
 print("Shape of non-imputed: ")
 print(cat_feature_pre.shape)
 # and the numerical features into another dataframe
-num_feature_pre = preMatrix.loc[:, num_cols].copy()
-print(num_feature_pre.shape)
+date_feature_pre = preMatrix.loc[:, date_cols].copy()
+print(date_feature_pre.shape)
 
 # SimpleImputer on the categorical features and apply a "missing_value" to NANs
 imputer_cat = SimpleImputer(strategy='constant', fill_value='missing_value')
 imputed_cat_feature_pre = pd.DataFrame(imputer_cat.fit_transform(cat_feature_pre))
 imputed_cat_feature_pre.columns = cat_feature_pre.columns
 
-# SimpleImputer on the numerical features and apply mode to NANs
-#imputer_num = SimpleImputer(strategy='most_frequent', verbose=5)
-#imputed_num_feature_pre = pd.DataFrame(imputer_num.fit_transform(num_feature_pre))
-#imputed_num_feature_pre.columns = num_feature_pre.columns
-imputed_num_feature_pre = num_feature_pre.copy()
-#print(num_feature_pre.isnull().sum())
-
-#for col in num_cols:
-#    imputed_num_feature_pre[col].replace(pd.NaT, imputed_num_feature_pre[col].mode().iloc[0])
-    #a = (imputed_num_feature_pre[col].mode())
-    #imputed_num_feature_pre[col].fillna(a)
+# SimpleImputer on the date features and apply mode to NANs
+imputer_date = SimpleImputer(strategy='most_frequent', verbose=5)
+imputed_date_feature_pre = pd.DataFrame(imputer_date.fit_transform(date_feature_pre))
+imputed_date_feature_pre.columns = date_feature_pre.columns
 
 print("Shape of imputed: ")
 print(imputed_cat_feature_pre.shape)
-print(imputed_num_feature_pre.shape)
-#print(imputed_num_feature_pre.isnull().sum())
+print(imputed_date_feature_pre.shape)
 
 imputed_combined = pd.merge(imputed_cat_feature_pre.reset_index(),
-                            imputed_num_feature_pre.reset_index(), how='left',
+                            imputed_date_feature_pre.reset_index(), how='left',
                             on='index')
-print('%s rows lost in merge' %(imputed_num_feature_pre.shape[0]-imputed_combined.shape[0]))
+print('%s rows lost in merge' %(imputed_date_feature_pre.shape[0]-imputed_combined.shape[0]))
 print(imputed_combined.shape)
 imputed_combined = imputed_combined.drop(columns = 'index')
 
-# apply the oneHotEncoder within prepare_inputs
-#X_train, X_test = prepare_inputs(X_train, X_test)
+# code to get x and y test/train df
+X_train = preMatrix.loc[train_ix]
+y_train = y1.loc[train_ix]
+
+X_test = preMatrix.loc[test_ix]
+y_test = y1.loc[test_ix]
