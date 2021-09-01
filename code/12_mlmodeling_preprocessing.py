227903a5e1e40bd7e69fd7f4f08eb24e3c0f8970
eunice30718 <81632872+eunice30718@users.noreply.github.com>
2021-09-02 02:12:33 +0800

    read in new data and make sure the code works

--- code/12_mlmodeling_preprocessing.py
+++ code/12_mlmodeling_preprocessing.py
@@ -12,18 +12,18 @@ import os
 # import matplotlib.pyplot as plt
 
 # ML imports
-# from sklearn.compose import ColumnTransformer
-# from sklearn.datasets import make_classification
-# from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.impute import SimpleImputer
-# from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
-# from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-# from sklearn.model_selection import cross_val_score
-# from sklearn.naive_bayes import MultinomialNB
-# from sklearn.svm import SVC
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.model_selection import GroupShuffleSplit
+from sklearn.compose import ColumnTransformer
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GroupShuffleSplit
 
 ##### Input Path
 DROPBOX_INT_PATH = '../../qss20_finalproj_rawdata/summerwork/'
@@ -33,9 +33,8 @@ TRLA_DATA_FILE_PATH = os.path.join(DROPBOX_INT_PATH, TRLA_DATA_FILE_NAME)
 GENERAL_DATA_FILE_PATH = os.path.join(DROPBOX_INT_PATH, GENERAL_DATA_FILE_NAME)
 
 # local path for now
-TRLA_DATA_FILE_PATH2 = "/Users/Firstclass/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/clean/whd_violations_wTRLA_catchmentonly.csv"
-GENERAL_DATA_FILE_PATH2 = "/Users/Firstclass/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/clean/whd_violations.csv"
-
+TRLA_DATA_FILE_PATH2 = "/Users/euniceliu/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/clean/whd_violations_wTRLA_catchmentonly.csv"
+GENERAL_DATA_FILE_PATH2 = "/Users/euniceliu/Dropbox (Dartmouth College)/qss20_finalproj_rawdata/summerwork/clean/whd_violations.csv"
 
 #### functions
 
@@ -88,8 +87,9 @@ pre_df = pd.read_csv(TRLA_DATA_FILE_PATH2)
 pre_df = pre_df.reset_index().copy()
 
 # read in h2a jobs data for filtering job feature using h2a df's column names
-with open(GENERAL_DATA_FILE_PATH2, "rb") as df:
-  h2a_data = pickle.load(df)
+h2a_data = pd.read_csv(GENERAL_DATA_FILE_PATH2)
+# with open(GENERAL_DATA_FILE_PATH2, "rb") as df:
+#   h2a_data = pickle.load(df)
 job_feature = h2a_data.columns.tolist()
 job_feature.append("jobs_group_id")
 
@@ -107,7 +107,7 @@ pre_df = pre_df.drop(columns=outcome_cols, axis=1)
 
 ## select columns that overlap with job_feature (col names from H2A_DATA)
 cols = [c for c in pre_df.columns if c in job_feature]
-preMatrix = pre_df[cols]
+preMatrix = pre_df[cols].copy()
 
 ## convert the dates to ordinal timestamps
 for col in ['JOB_END_DATE','JOB_START_DATE']:
@@ -139,7 +139,6 @@ print(selected_cat_large)
 for col in ['ATTORNEY_AGENT_CITY','ATTORNEY_AGENT_NAME','WORKSITE_CITY']:
     preMatrix = gen_topk_dummies(preMatrix,col,0.01)
 
-
 ##### do a train test split
 # split into train and test sets (80/20) -> returns index of the split, not the split df itself
 # group shuffle split on job group id: 'jobs_group_id'
@@ -148,7 +147,6 @@ train_ix, test_ix = next(gs.split(preMatrix, y1, groups=preMatrix.jobs_group_id)
 
 
 #### data imputation
-
 # get the categorical features in one dataframe
 cat_feature_pre = preMatrix.loc[:, selected_cat_large].copy()
 print("Shape of non-imputed: ")
@@ -191,59 +189,3 @@ y_train = y1.loc[train_ix]
 X_test = imputed_dummy_df.loc[test_ix]
 y_test = y1.loc[test_ix]
 
-#### modeling
-
-## functions
-def get_metrics(y_test, y_predicted):
-    '''generate evaluation scores accroding predicted y'''
-    accuracy = accuracy_score(y_test, y_predicted)
-    precision = precision_score(y_test, y_predicted, average='binary')
-    recall = recall_score(y_test, y_predicted, average='binary')
-    f1 = f1_score(y_test, y_predicted, average='binary')
-    return accuracy, precision, recall, f1
-
-def estimate_models(model_list, X_train, y_train, X_test, y_test):
-    '''takes in a list of models, fit and test each one, generates a df with evaluation parameters of all models ran'''
-    evals_array = []
-    for i in range(0, len(model_list)):
-        ## pull out model
-        one_model = model_list[i]
-
-        print("fitting model: " + str(one_model))
-        ## fit the model and evaluate
-        one_model.fit(X_train, y_train)
-        y_pred = one_model.predict(X_test)
-
-        print("Confusion matrix \n")
-        print(pd.crosstab(pd.Series(y_test, name='Actual'), pd.Series(y_pred, name='Predicted')))
-
-        accuracy, precision, recall, f1 = get_metrics(y_test, y_pred)
-        print("accuracy = %.3f \nprecision = %.3f \nrecall = %.3f \nf1 = %.3f" % (accuracy, precision, recall, f1))
-
-        evals_array.append(pd.DataFrame({'Model': [model_list[i]],
-                                     'Accuracy': [accuracy], 'Precision': [precision], 'Recall':[recall], 'f1-score':[f1]
-                                      }))
-
-    evals_df = pd.concat(evals_array)
-    return evals_df
-
-## create a list of model objects
-model_list1 = [DecisionTreeClassifier(random_state=0, max_depth = 5),
-                    DecisionTreeClassifier(random_state=0, max_depth = 50)]
-model_list = [DecisionTreeClassifier(random_state=0, max_depth = 5),
-                    DecisionTreeClassifier(random_state=0, max_depth = 50),
-                    RandomForestClassifier(n_estimators = 100, max_depth = 20),
-                    RandomForestClassifier(n_estimators = 1000, max_depth = 20),
-                    GradientBoostingClassifier(criterion='friedman_mse', n_estimators=100),
-                    GradientBoostingClassifier(criterion='friedman_mse', n_estimators=1000),
-                AdaBoostClassifier(),
-                LogisticRegression(max_iter=10000),
-                LogisticRegressionCV(max_iter=10000),
-                LogisticRegression(penalty = "l1",max_iter=10000),
-                LogisticRegressionCV(solver = "liblinear",
-                                 penalty = "l1", max_iter=10000)]
-print("Length of classifier list is:" + str(len(model_list)))
-
-evals_df = estimate_models(model_list, X_train, y_train, X_test, y_test)
-evals_df.to_csv("model_evaluation.csv", encoding='utf-8', index=False)
-print(evals_df)
